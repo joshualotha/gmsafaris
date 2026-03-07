283a2de0827b43f38d7a4193bb32829627068d4e
codeshinobii <iamjoshualotha@gmail.com>
2026-03-07 22:28:15 +0300

    fix: Fully disable PageSpeed in .htaccess and add defensive checks for JavaScript library initializations.

--- js/main.js
+++ js/main.js
@@ -13,24 +13,28 @@
 
 
     // Initiate the wowjs
-    new WOW().init();
+    $(document).ready(function () {
+        if (typeof WOW === 'function') {
+            new WOW().init();
+        }
+    });
 
 
-   // Back to top button
-   $(window).scroll(function () {
-    if ($(this).scrollTop() > 300) {
-        $('.back-to-top').fadeIn('slow');
-    } else {
-        $('.back-to-top').fadeOut('slow');
-    }
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
     });
     $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
         return false;
     });
 
     // Hero Carousel
-    $(document).ready(function() {
+    $(document).ready(function () {
         try {
             $(".hero-carousel").owlCarousel({
                 items: 1,
@@ -40,7 +44,7 @@
                 autoplayHoverPause: true,
                 dots: true,
                 nav: true,
-                navText: ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
+                navText: ["<i class='fa fa-chevron-left'></i>", "<i class='fa fa-chevron-right'></i>"],
                 animateOut: 'fadeOut',
                 animateIn: 'fadeIn',
                 smartSpeed: 1000,
@@ -69,9 +73,13 @@
 
 
     // Facts counter
-    $('[data-toggle="counter-up"]').counterUp({
-        delay: 10,
-        time: 2000
+    $(document).ready(function () {
+        if ($.fn.counterUp) {
+            $('[data-toggle="counter-up"]').counterUp({
+                delay: 10,
+                time: 2000
+            });
+        }
     });
 
 
@@ -86,10 +94,10 @@
         autoplaySpeed: 10000,
         autoplayHoverPause: false,
         responsive: {
-            0:{ items:1 },
-            575:{ items:1 },
-            767:{ items:2 },
-            991:{ items:3 }
+            0: { items: 1 },
+            575: { items: 1 },
+            767: { items: 2 },
+            991: { items: 3 }
         }
     });
 
@@ -104,10 +112,10 @@
         autoplaySpeed: 10000,
         autoplayHoverPause: false,
         responsive: {
-            0:{ items:1 },
-            575:{ items:1 },
-            767:{ items:2 },
-            991:{ items:3 }
+            0: { items: 1 },
+            575: { items: 1 },
+            767: { items: 2 },
+            991: { items: 3 }
         }
     });
 
